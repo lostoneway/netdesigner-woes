d96461fe654f36e549ea967fa8fc4938c1d4c150
lostoneway <abishop416@gmail.com>
2022-07-19 19:01:08 -0500

    wrote logic for edit/delete on server side

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.use(express.json())
 app.use(cors())
 
 //Routes
+//GET or READ METHOD
 app.get('/', (request,response) => {
     db.collection('netdesigner').find().toArray()
     .then(data => {
@@ -39,9 +40,16 @@ app.get('/', (request,response) => {
     .catch(error => response.status(500).send({message: error.message}))
 })
 
+//POST or CREATE METHOD
 app.post('/addWoe', (request, response) => {
     // console.log(req.body)
-    db.collection('netdesigner').insertOne({name: request.body.name, issue: request.body.issue, wishes: request.body.wishes, actionTaken: request.body.actionTaken, solution: request.body.solution}) 
+    db.collection('netdesigner').insertOne({ 
+        name: request.body.name, 
+        issue: request.body.issue, 
+        wishes: request.body.wishes, 
+        actionTaken: request.body.actionTaken, 
+        solution: request.body.solution
+    }) 
     .then(result => {
         console.log('NetDesigner Woe Added')
         response.redirect('/')
@@ -49,6 +57,47 @@ app.post('/addWoe', (request, response) => {
     .catch(error => response.status(500).send({message: error.message}))
   })
 
+//EDIT or UPDATE METHOD
+app
+  .route('/edit/:id')
+  .get((request,response)=> {
+    const id = request.params.id; 
+    db.collection('netdesigner').find().toArray()
+    .then(data => {
+        response.render('edit.ejs', {info: data, idInfo: id})
+    }) 
+    .catch(error => response.status(500).send({message: error.message}))
+  })
+  .post((request,response) => {
+    const id = request.params.id;
+    db.connection('netdesigner').findByIdAndUpdate(
+        id, 
+        {
+            name: request.body.name, 
+            issue: request.body.issue, 
+            wishes: request.body.wishes, 
+            actionTaken: request.body.actionTaken, 
+            solution: request.body.solution 
+        })
+        .then(result => {
+            console.log('NetDesigner Woe Updated')
+            response.redirect('/')
+        })
+        .catch(error => response.status(500).send({message: error.message}))
+  })
+
+//DELETE
+app
+  .route('/remove/:id')
+  .get((request,response)=>{
+    const id = request.params.id;
+    db.connection('netdesigner').findByIdAndRemove(id)
+    .then(result => {
+        console.log('NetDesigner Woe Deleted')
+        response.redirect('/')
+    }) 
+    .catch(error => response.status(500).send({message: error.message}))
+  })
 //setup listening port 
 app.listen(process.env.PORT || 3000, () => {
     //check that your server is running during development
