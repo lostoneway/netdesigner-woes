fffa738b1a296b3c576efc36f26364adefbcce66
lostoneway <abishop416@gmail.com>
2022-07-23 17:59:22 -0500

    created models folder and schema

--- server.js
+++ server.js
@@ -6,17 +6,13 @@ const cors = require('cors')
 // const MongoClient = require('mongodb').MongoClient
 const mongoose = require('mongoose')
 require('dotenv').config()
-//add model variable 
+const NetdesignerTask = require('./models/netdesignerTask')
 
 //variables
 let db,     
     dbConnectionString = process.env.DB_STRING,
     dbName = 'NetdesignerWoesDemo',
-    collection = 'tasks'
-
-//connect to db
-mongoose.connect(dbConnectionString,{useNewUrlParser: true},
-    () => {console.log('Connected to Database')})
+    collection
 
 //Middlewares 
 app.set('view engine','ejs')
@@ -25,12 +21,45 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(cors())
 
+//connect to db
+mongoose.connect(dbConnectionString,{useNewUrlParser: true},
+    () => {console.log('Connected to Database')})
+
+
 //Routes
 //GET or READ METHOD
-
+app.get('/', async (req, res) => {
+    try {
+        NetdesignerTask.find({},(error,tasks)=>{
+            res.render('index.ejs',{
+                netdesignerTasks: tasks
+            })
+        })
+    } catch (error) {
+        res.status(500).send({message: error.message})
+    }
+})
 
 //POST or CREATE METHOD
-
+app.post('/', async (req,res) => {
+    const netdesignerTask = new NetdesignerTask( 
+        {
+            name: req.body.name,
+            issue: req.body.issue,
+            wishes: req.body.wishes,
+            actionTaken: req.body.actionTaken,
+            solution: req.body.solution
+        }
+    )
+    try{
+        await netdesignerTask.save()
+        console.log(netdesignerTask)
+        res.redirect('/')
+    } catch (error){
+        if(error)res.status(500).send({message: error.message})
+        res.redirect('/')
+    }
+})
 
 //EDIT or UPDATE METHOD
 
