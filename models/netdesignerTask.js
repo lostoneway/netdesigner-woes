2234b82c57038e94ff0e2ee28c20405b16da6711
lostoneway <abishop416@gmail.com>
2022-07-25 17:22:12 -0500

    marked required in ejs and not mongoose for clear UI

--- models/netdesignerTask.js
+++ models/netdesignerTask.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 const netdesignerTaskSchema = new mongoose.Schema({
     name:{
-        type: String,
-        required: true
+        type: String
+        // required: true
     },
     issue: {
-        type: String,
-        required: true
+        type: String
+        // required: true
     },
     wishes: {
         type: String
